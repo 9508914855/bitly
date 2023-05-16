d15ad7a67d35727d4bea134c603f54e5c65a41f8
Shashi Kumar <121589680+9508914855@users.noreply.github.com>
2023-05-16 21:03:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ function shortenURL() {
   var input = document.getElementById("url-input").value;
   // Implement your logic here to shorten the URL and store it
   // You can use a service like Bitly API or create your own URL shortening algorithm
-  var shortenedURL = "https://9508914855.github.io/bitly";
+  
+  // Generate a unique timestamp-based identifier
+  var timestamp = Date.now();
+  
+  // Create the shortened URL by combining the base URL with the timestamp identifier
+  var shortenedURL = "https://9508914855.github.io/bitly/" + timestamp;
+  
   document.getElementById("shortened-url").innerHTML = shortenedURL;
 }
