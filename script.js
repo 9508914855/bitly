dbc071b897760f01d0da96135207fad3be0b7717
Shashi Kumar <121589680+9508914855@users.noreply.github.com>
2023-05-16 20:53:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ function shortenURL() {
   var input = document.getElementById("url-input").value;
   // Implement your logic here to shorten the URL and store it
   // You can use a service like Bitly API or create your own URL shortening algorithm
-  var shortenedURL = "http://yourusername.github.io/your-shortened-url";
+  var shortenedURL = "https://9508914855.github.io/bitly";
   document.getElementById("shortened-url").innerHTML = shortenedURL;
 }
